be4b0c4db50f463020dd19b4027633458c588801
Victor Martins Dias <victormartinsjob@gmail.com>
2026-05-11 07:22:00 -0300

    feat: add formatted currency amount element to DOM list item

--- src/main.js
+++ src/main.js
@@ -78,9 +78,13 @@ function expenseAdd(newExpense) {
     const expenseCategory = document.createElement("span");
     expenseCategory.textContent = newExpense.category_name;
 
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`;
+
     expenseInfo.append(expenseName, expenseCategory);
     expenseIcon.appendChild(useEl);
-    expenseItem.append(expenseIcon, expenseInfo);
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
     expenseList.append(expenseItem);
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
